4e4a1454a8ad58eb1756b81dc096fe67f35d7746
rebeccarj-im <154825190+rebeccarj-im@users.noreply.github.com>
2025-07-03 13:16:59 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5050',
-      '/proxy-image': 'http://localhost:5050',
-    },
-  },
+  
 });
