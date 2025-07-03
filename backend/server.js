f698d5edeb881a3a296ebbb99e516e977e36b9e9
rebeccarj-im <154825190+rebeccarj-im@users.noreply.github.com>
2025-07-03 13:35:36 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 5050;
-const NASA_KEY = process.env.NASA_KEY || 'DEMO_KEY';
+const NASA_KEY = process.env.NASA_KEY ;
 
 app.use(cors());
 app.use(express.json());
@@ -41,7 +41,7 @@ app.get('/proxy-image', async (req, res) => {
 // NASA Image Library pic/video
 app.get('/api/card1', async (req, res) => {
   try {
-    const url = `https://images-api.nasa.gov/search?q=earth&media_type=image,video`;
+    const url = `https://images-api.nasa.gov/search?q=earth&media_type=image`;
     const response = await axios.get(url);
     const items = response.data.collection.items.slice(0, 18);
 
